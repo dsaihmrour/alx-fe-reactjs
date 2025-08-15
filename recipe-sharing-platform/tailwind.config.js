a70ef7a0d72b1495806374a279fec3919e25d3d0
Sigurd <synchasgard@gmail.com>
2025-08-15 16:29:49 +0100

    Update tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+module.exports = {
+  content: [
+    './index.html',
+    './public/index.html',       // add this line
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
     extend: {},
   },
